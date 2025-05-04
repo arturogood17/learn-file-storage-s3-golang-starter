542e3caf210d76ecb8a80cb9bc1118c6fd72afdb
arturogood17 <arturo_good17@hotmail.com>
2025-05-04 23:54:46 +0200

    Updated handler_upload_thumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,48 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20 //10 mb
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse form file", err)
+		return
+	}
+	mediaType := header.Header.Get("Content-Type")
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Media type header not set", err)
+		return
+	}
+	mediaData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read file", err)
+		return
+	}
+	defer file.Close()
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video in database", err)
+		return
+	}
+	if metadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Can't change the video as you are not its owner", nil)
+		return
+	}
+	videoThumbnails[videoID] = thumbnail{
+		data:      mediaData,
+		mediaType: mediaType,
+	}
+	newURL := fmt.Sprintf("http://localhost%s/api/thumbnails/%s", cfg.port, videoIDString)
+	metadata.ThumbnailURL = &newURL
+	err = cfg.db.UpdateVideo(metadata)
+	if err != nil {
+		delete(videoThumbnails, videoID)
+		respondWithError(w, http.StatusInternalServerError, "Error updating video metadata in database", nil)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, metadata)
 }
